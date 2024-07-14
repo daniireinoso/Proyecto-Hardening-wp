d6c4e3b5c9b7062a839ffe437722bbec43899164
Daniela <daniireinoso@gmail.com>
2024-07-14 18:13:25 -0500

    Primer commit

---
+++ html/wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1721544080;s:7:"authKey";s:64:"77#mn|izgiZ~g:<T.qi1q3kXWT05:=28q_]7;8}Qb4`kSZIerKO)c.cMv9w@6DQJ";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1301;}
